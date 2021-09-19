8e767d23ff731c30d1ba86c73f733c2789ff6197
mohajerimasoud <masoudmohajeri.wd@gmail.com>
2021-09-19 17:56:15 +0430

    Add Fonts

--- src/App.vue
+++ src/App.vue
@@ -5,14 +5,33 @@
 </template>
 
 <script lang="ts">
-import { IonApp, IonRouterOutlet } from '@ionic/vue';
-import { defineComponent } from 'vue';
+import { IonApp, IonRouterOutlet } from "@ionic/vue";
+import { defineComponent } from "vue";
 
 export default defineComponent({
-  name: 'App',
+  name: "App",
   components: {
     IonApp,
-    IonRouterOutlet
-  }
+    IonRouterOutlet,
+  },
 });
 </script>
+
+<style >
+@font-face {
+  font-family: "Vazir";
+  font-weight: bold;
+  src: url("./assets/Fonts/Vazir-Bold-UI.ttf");
+}
+
+@font-face {
+  font-family: "Vazir";
+  src: url("./assets/Fonts/Vazir-Regular-UI.ttf");
+}
+
+@import url("https://fonts.googleapis.com/css2?family=Roboto:wght@400;700&display=swap");
+
+* {
+  font-family: "Vazir", "Roboto", sans-serif;
+}
+</style>
